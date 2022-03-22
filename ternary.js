c31858fdd1509127197728fbf43fb2c37376ea0e
nnoranna <anagy.nora.anna@gmail.com>
2022-03-22 17:13:44 +0100

    Practice ternary operator

---
+++ ternary.js
@@ -0,0 +1,12 @@
+const [min, max] = [5, 30];
+
+/* if (min < max) {
+    console.log("Min lower than max");
+} else {
+    console.log("Min is not lower than max");
+} */
+
+/* min < max ? console.log("Min lower than max") : console.log("Min is not lower than max"); */
+
+
+console.log(`Min ${min < max ? "" : "not"}lower than max`);
